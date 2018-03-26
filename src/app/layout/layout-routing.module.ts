330e7f0bfcdfa4dd174bff8592a9c2145e14c958
victor <victor.basset@yahoo.fr>
2018-03-26 17:33:14 +0200

    J'ai clean le code pour garder uniquement les éléments nécessaires.

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -15,11 +15,6 @@ const routes: Routes = [
             { path: 'add-space', loadChildren: './add-space/add-space.module#AddSpaceModule' },
             { path: 'update-parking', loadChildren: './update-parking/update-parking.module#UpdateParkingModule' },
             { path: 'parking-spaces', loadChildren: './parking-spaces/parking-spaces.module#ParkingSpacesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
     }
 ];
