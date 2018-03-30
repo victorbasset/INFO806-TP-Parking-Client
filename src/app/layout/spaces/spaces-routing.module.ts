e211e1da58f5e3662e2091aca1bc771428718433
victor <victor.basset@yahoo.fr>
2018-03-30 16:37:31 +0200

    Ajout de l'update et delete des spaces. On peut maintenant commencer la logique Rest.

--- src/app/layout/spaces/spaces-routing.module.ts
+++ src/app/layout/spaces/spaces-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ParkingSpacesComponent } from './parking-spaces.component';
+import { SpacesComponent } from './spaces.component';
 
 const routes: Routes = [
     {
-        path: '', component: ParkingSpacesComponent
+        path: '', component: SpacesComponent
     }
 ];
 
@@ -12,5 +12,5 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class ParkingSpacesRoutingModule {
+export class SpacesRoutingModule {
 }
