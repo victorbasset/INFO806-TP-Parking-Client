0d3c0031a6b10cba84c71a6ddf7a88bc0dff50a1
Victor <victor.basset@yahoo.fr>
2018-04-02 18:37:23 +0200

    Début du Rest

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../router.animations';
+import {ServiceRestUser} from '../../service/service.rest.user';
 
 @Component({
     selector: 'app-login',
@@ -9,11 +10,17 @@ import { routerTransition } from '../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(public router: Router) {}
+    constructor(public router: Router,private serviceRestUser: ServiceRestUser) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+      console.log("here");
+      this.serviceRestUser.all().then(
+        result => {console.log(result);},
+        error => {console.log(error);}
+      );
+    }
 
     onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
+      localStorage.setItem('isLoggedin', 'true');
     }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ServiceRestUser } from '../../service/service.rest.user';
 
 @NgModule({
     imports: [CommonModule, LoginRoutingModule],
-    declarations: [LoginComponent]
+    declarations: [LoginComponent],
+    providers: [ServiceRestUser]
 })
 export class LoginModule {}
